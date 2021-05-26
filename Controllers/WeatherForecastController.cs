7db07057d514312b409888f539b6e04d430a1a89
Raaed Khan <raaedk@gmail.com>
2021-05-26 10:20:36 -0400

    implemented query and headers routes

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RequestsResponses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        // private static readonly string[] Summaries = new[]
+        // {
+        //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        // };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/headers")]
+        public IActionResult GetHeaders()
+        {
+            var headers = base.Request.Headers;
+            return base.Ok(headers);
+        }
+
+        [HttpGet]
+        [Route("/query")]
+        public IActionResult GetQuery()
+        {
+            var test = Request.QueryString.ToString();
+
+            return base.Ok(test);
+        }
+    }
+}
